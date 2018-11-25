6cf9986c73a7aa82156ca7f51ff0b78eb73f8560
Piotr Surdyn <piotrsurdyn@gmail.com>
2018-11-25 16:48:01 +0100

    Zaimportowana kalsa Student i zrobiony get na kontrolerze Students

---
+++ Backend/StudentsApp/StudentsApp.Backend/Controllers/StudentsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentsApp.Backend.Models;
+
+namespace StudentsApp.Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly s13619Context _context =  new s13619Context();
+
+        public StudentsController(s13619Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("getStudents")]
+        public IActionResult GetStudents()
+        {
+            //Content negotiation
+            //Accept-type: json;q=0.9;xml;q=0.5
+            //select * from students
+            return Ok(_context.Student.ToList());
+        }
+    }
+}
---
+++ Backend/StudentsApp/StudentsApp.Backend/Models/Student1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudentsApp.Backend.Models
+{
+    public partial class Student
+    {
+        public int IdStudent { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string IndexNumber { get; set; }
+    }
+}
